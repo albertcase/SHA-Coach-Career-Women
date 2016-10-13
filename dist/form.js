d9416b7b0d0cacc82d0372b6920ecdf547b78e0f
evelyn_zhang <1054982653@qq.com>
2016-10-13 16:45:38 +0800

    form js

--- dist/form.js
+++ dist/form.js
@@ -195,48 +195,6 @@ var Zepto=function(){function L(t){return null==t?String(t):j[S.call(t)]||"objec
 
 
 
-$(document).ready(function(){
-    function weixinshare(obj){
-        wx.ready(function(){
-            wx.onMenuShareAppMessage({
-                title: obj.title1,
-                desc: obj.des,
-                link: obj.link,
-                imgUrl: obj.img,
-                type: '',
-                dataUrl: '',
-                success: function () {
-                    console.log('share success to friend');
-
-                },
-                cancel: function () {
-
-                }
-            });
-            wx.onMenuShareTimeline({
-                title: obj.title1,
-                link: obj.link,
-                imgUrl: obj.img,
-                success: function () {
-                    console.log('share success to timeline');
-                },
-                cancel: function () {
-
-                }
-            });
-
-
-        })
-    }
-
-    weixinshare({
-        title1: '为梦想，你包容了什么？',
-        des: '参与心理测试赢取由COACH追梦女性倾情赞助的礼物',
-        link: window.location.href,
-        img: 'http://careerwomen.samesamechina.com/dist/images/share.jpg'
-    })
-
-});
 ;(function(){
 	var ua = navigator.userAgent.toLowerCase();
 	var Common = {
@@ -452,6 +410,48 @@ $(document).ready(function(){
 
 
 
+$(document).ready(function(){
+    function weixinshare(obj){
+        wx.ready(function(){
+            wx.onMenuShareAppMessage({
+                title: obj.title1,
+                desc: obj.des,
+                link: obj.link,
+                imgUrl: obj.img,
+                type: '',
+                dataUrl: '',
+                success: function () {
+                    console.log('share success to friend');
+
+                },
+                cancel: function () {
+
+                }
+            });
+            wx.onMenuShareTimeline({
+                title: obj.title1,
+                link: obj.link,
+                imgUrl: obj.img,
+                success: function () {
+                    console.log('share success to timeline');
+                },
+                cancel: function () {
+
+                }
+            });
+
+
+        })
+    }
+
+    weixinshare({
+        title1: '为梦想，你包容了什么？',
+        des: '参与心理测试赢取由COACH追梦女性倾情赞助的礼物',
+        link: window.location.href,
+        img: 'http://careerwomen.samesamechina.com/dist/images/share.jpg'
+    })
+
+});
 //redpacket
 ;(function(){
     'use strict';
