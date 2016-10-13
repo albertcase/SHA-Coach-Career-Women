a8d6d3ef25dfcc463a9ba3af4e9e792504a272b0
evelyn_zhang <1054982653@qq.com>
2016-10-13 16:19:08 +0800

    share

--- assets/js/wxshare.js
+++ assets/js/wxshare.js
@@ -1,13 +1,6 @@
 $(document).ready(function(){
     function weixinshare(obj){
         wx.ready(function(){
-
-            window.wechat_setting.friend  = {
-                title: obj.title1,
-                desc: obj.des,
-                link: obj.link,
-                imgUrl: obj.img
-            };
             wx.onMenuShareAppMessage({
                 title: obj.title1,
                 desc: obj.des,
@@ -23,13 +16,6 @@ $(document).ready(function(){
 
                 }
             });
-
-
-            window.wechat_setting.timeline = {
-                title: obj.title1,
-                link: obj.link,
-                imgUrl: obj.img
-            };
             wx.onMenuShareTimeline({
                 title: obj.title1,
                 link: obj.link,
@@ -50,7 +36,7 @@ $(document).ready(function(){
         title1: '为梦想，你包容了什么？',
         des: '参与心理测试赢取由COACH追梦女性倾情赞助的礼物',
         link: window.location.href,
-        img: 'http://coachxmas.samesamechina.com/img/share.jpg'
+        img: 'http://careerwomen.samesamechina.com/dist/images/share.jpg'
     })
 
 });
