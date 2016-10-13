c08247c52cd92d2a8035bde64ddfdd99f6f8f440
Demon Zhang <demon.zhang@samesamechina.com>
2016-10-13 17:29:44 +0800

    no message

--- bundle/CoachBundle/SiteController.php
+++ bundle/CoachBundle/SiteController.php
@@ -6,33 +6,9 @@ use Core\Controller;
 
 class SiteController extends Controller {
 
-	public function indexAction() {		
-		$UserAPI = new \Lib\UserAPI();
-		$user = $UserAPI->userLoad(true);
-		if (!$user) {
-			$parameterAry = $_GET;
-			if(count($parameterAry)>0)
-				$url = "/?".http_build_query($parameterAry);
-			else
-				$url = "/";
-			$_SESSION['redirect_url'] = $url;
-			$WechatAPI = new \Lib\WechatAPI();
-			$WechatAPI->wechatAuthorize();
-		}
-		if (!isset($user->headimgurl)) {
-			$databaseapi = new \Lib\DatabaseAPI();
-			$user->headimgurl = $databaseapi->getHeadimgurlByOpenid($user->openid);
-			$_SESSION['user'] = $user;
-		}
-		$wechatapi = new \Lib\WechatAPI();
-		$rs = $wechatapi->isSubscribed($user->openid); 
-		if ($rs) {
-			$this->render('index', array('headimgurl' => $user->headimgurl));
-			exit;
-		}
-		$this->render('qrcode');
+	public function indexAction() {	
+	echo 1;	
 		exit;
-		
 	}
 
 }
